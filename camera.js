8a75126310e3ce4745e1e8288b1e56f06ee2e4df
Marko <stankovicmarko333@gmail.com>
2020-03-12 08:52:55 +0100

    proba

--- camera.js
+++ camera.js
@@ -14,6 +14,21 @@ function take_snapshot() {
    // take snapshot and get image data
    Webcam.snap( function(data_uri) {
     // display results in page
-    document.getElementById('results').innerHTML = '<img src="'+data_uri +'"/>';
+    document.getElementById('results').innerHTML = '<img id="imageprev" src="'+data_uri +'"/>';
     } );
 }
+
+
+
+function saveSnap(){
+    // Get base64 value from <img id='imageprev'> source
+    var base64image = document.getElementById("imageprev").src;
+   
+    // Webcam.upload( base64image, 'upload.php', function(code, text) {
+    //  console.log('Save successfully');
+    //  //console.log(text);
+    // });
+    console.log('Save successfully');
+    console.log(base64image);
+   
+   }
